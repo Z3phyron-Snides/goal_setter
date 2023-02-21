4beb6dedd795a420c3298eee0146c069ea81a33c
z3phyron <z3phyronsnides@gmail.com>
2023-02-21 21:58:36 +0100

    serve client

--- app/src/common/layouts/root.jsx
+++ app/src/common/layouts/root.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import {  Outlet } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Root = () => {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-
+  useEffect(() => {
+    if (!user?.token && !location.pathname.includes("/auth")) {
+      navigate("/auth/signin");
+    }
+  }, [user, location, navigate]);
 
   return (
     <div>
-     <Outlet />
+      <Outlet />
     </div>
   );
 };
--- src/app/index.js
+++ src/app/index.js
@@ -2,26 +2,39 @@ const express = require("express");
 const { errorHandler } = require("../middlewares/errorMiddleware");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const cors = require('cors');
+const cors = require("cors");
+const path = require('path');
 const goalRoutes = require("../routes/goalRoutes");
 const authRoutes = require("../routes/authRoutes");
 const userRoutes = require("../routes/userRoutes");
-const rootRoutes = require("../routes/root");
+// const rootRoutes = require("../routes/root");
 const connectDB = require("../config/db");
 
 const app = express();
 
 connectDB();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use("/", rootRoutes);
+// app.use("/", rootRoutes);
 app.use("/api/goals", goalRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
+//serve client
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../app/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../../", "app", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production!!!"));
+}
 
 app.use(errorHandler);
 
